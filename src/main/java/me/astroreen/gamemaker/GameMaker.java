218e412bd9c15b6f49fbeb8db835ae6609d10c83
Astroreen <ilja.zoludev@gmail.com>
2024-02-19 03:34:05 +0200

    Added couple of things

--- src/main/java/me/astroreen/gamemaker/GameMaker.java
+++ src/main/java/me/astroreen/gamemaker/GameMaker.java
@@ -1,17 +1,54 @@
 package me.astroreen.gamemaker;
 
+import me.astroreen.astrolibs.api.bukkit.listener.ListenerManager;
+import me.astroreen.astrolibs.api.bukkit.logger.LoggerFactory;
+import me.astroreen.astrolibs.api.common.AstroLibsAPI;
+import me.astroreen.astrolibs.api.common.PluginTemplate;
+import me.astroreen.astrolibs.api.common.config.file.Configuration;
+import me.astroreen.astrolibs.api.common.logger.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class GameMaker extends JavaPlugin {
+import java.io.File;
+import java.io.IOException;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+public final class GameMaker extends JavaPlugin implements PluginTemplate {
 
+  private static GameMaker instance;
+  private static Logger log;
+  private static Configuration config;
+
+  @Override
+  public void onEnable() {
+    log = LoggerFactory.create(this);
+    ListenerManager.setup(this, null, log);
+
+    try {
+      config =
+          AstroLibsAPI.getInstance()
+              .createConfigurationFile(
+                  getClass().getClassLoader().getResourceAsStream("config.yml"),
+                  new File(getInstance().getDataFolder(), "config.yml"));
+    } catch (IOException e) {
+      log.error("Could not create configuration file. Default values will be used instead.", e);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    instance = this; // the moment everything works
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+
+  public static GameMaker getInstance() {
+    return instance;
+  }
+
+  public static Logger log() {
+    return log;
+  }
+
+  public static Configuration getConfiguration() {
+    return config;
+  }
 }
